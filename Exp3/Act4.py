da773f0eeeabca19d326377ee83ce2299a18ef5f
OmGate07 <gateom07@gmail.com>
2026-03-16 10:31:03 +0530

    Update Act4.py

--- Exp3/Act4.py
+++ Exp3/Act4.py
@@ -4,3 +4,18 @@ Created on Mon Feb 16 10:09:34 2026
 
 @author: Om Gate
 """
+# Recursive function to calculate total stairs climbed
+
+def count_stairs(steps):
+    if steps == 0:
+        return 0
+    else:
+        return 1 + count_stairs(steps - 1)
+
+# Taking input from user
+total_steps = int(input("Enter number of steps climbed: "))
+
+# Function call
+result = count_stairs(total_steps)
+
+print("Total stairs climbed:", result)
